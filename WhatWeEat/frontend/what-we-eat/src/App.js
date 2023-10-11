13dfeb3fac36d743cfd7626e3bc9e1576239bbd1
Ziyang Deng <zd2014@nyu.edu>
2023-10-11 17:05:51 -0400

    Forms added
    Login page, signup page, and demo dashboard page

--- WhatWeEat/frontend/what-we-eat/src/App.js
+++ WhatWeEat/frontend/what-we-eat/src/App.js
@@ -1,30 +1,49 @@
 // import logo from './logo.svg';
 // import './App.css';
+import ReactDOM from "react-dom/client";
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import CreateUser from './createUser';
+import SignInSide from './Forms/SignInSide';
+import SignUp from './Forms/SignUp';
+import Dashboard from './Forms/Dashboard';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+
 
 function App() {
     const [data, setData] = useState(null);
 
     useEffect(() => {
         axios.get('http://localhost:8000/')
-             .then(response => {
-                 setData(response.data);
-             })
-             .catch(error => {
-                 console.error("Error fetching data: ", error);
-                 setData(null);
-             });
-    }, []);  
+            .then(response => {
+                setData(response.data);
+            })
+            .catch(error => {
+                console.error("Error fetching data: ", error);
+                setData(null);
+            });
+    }, []);
 
     return (
-        <div>
-            {data ? JSON.stringify(data) : "Welcome to What We Eat"}
-            <CreateUser /> 
-        </div>
+        <>
+            {/* {data ? JSON.stringify(data) : "Welcome to What We Eat"}
+                <CreateUser /> */}
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                    {/* <Route exact path="/" component={ () => <Dashboard user={this.state.user}/> } /> */}
+                    <Route path="/login" element={<SignInSide />} />
+                    <Route path="/signup" element={<SignUp />} />
+                    {/* Add other routes as needed */}
+                </Routes>
+            </BrowserRouter>
+        </>
     );
 }
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
 
 // function App() {
 //   return (
